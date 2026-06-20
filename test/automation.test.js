a8ced6442f0667dd2baaeb6b8ae56502423c35e9
lkmnaji <setyoaji.lukman@gmail.com>
2026-06-21 00:09:06 +0700

    update_bug_fix_post_automation

--- test/automation.test.js
+++ test/automation.test.js
@@ -28,7 +28,7 @@ describe('sampel automation method get', () => {
             .set('Authorization', 'Basic aWx1bWFfZGV2ZWxvcG1lbnRfRlg0ZjBNNXN4RGdrNXFGeVpuazYwWmVuZ0FmQTlvMzF4M2VjZDI5dmloamM0Vmh5SjhGY2xaaEhqanc6')
             .expect(200);
 
-        console.log("Cek hasil disini:", response.body)
+        console.log("Cek hasil get disini:", response.body)
 
         expect(response.body.id).to.equal('bknv_6a341b6e2471702471a92c6f');
         expect(response.body.bank_code).to.equal('BCA');
@@ -37,42 +37,52 @@ describe('sampel automation method get', () => {
         const valid = validate(response.body);
         
         if (!valid) {
-            console.log("Error Details:", validate.errors);
+            console.log("Error GET Details:", validate.errors);
         }
         expect(valid).to.be.true;
     });
 
-    // it('cek method post', async () => {
-    //     const postSchema = {
-    //         type: "object",
-    //         properties: {
-    //             name: { type: "string" },
-    //             job: { type: "string" },
-    //             id: { type: "string" },
-    //             createdAt: { type: "string" }
-    //         },
-    //         required: ["name", "job", "id", "createdAt"]
-    //     };
+    it('cek method post', async () => {
+        const postSchema = {
+            type: "object",
+            properties: {
+                id: { type: "string" },
+                status: { type: "string" },
+                reference_id: { type: "string" },
+                bank_code: { type: "string" },
+                bank_account_number: { type: "string" },
+                account_holder_name: { type: "string" } 
+            },
+            required: ["reference_id", "status", "bank_code", "bank_account_number"]
+        };
 
-    //     const requestBody = {
-    //         name: "jerry",
-    //         job: "Quality Assurance"
-    //     };
+        const requestBody = {
+            "bank_account_number": "84392849",
+            "bank_code": "MANDIRI",
+            "given_name": "Saptono",
+            "surname": "Saptono",
+            "reference_id" : "testing123-456"
+        };
 
-    //     const response = await request(baseUrl)
-    //         .post('/users')
-    //         .send(requestBody)
-    //         .expect(201);
+        const response = await request(baseUrl)
+            .post('/identity/bank_account_data_requests')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', 'Basic aWx1bWFfZGV2ZWxvcG1lbnRfRlg0ZjBNNXN4RGdrNXFGeVpuazYwWmVuZ0FmQTlvMzF4M2VjZDI5dmloamM0Vmh5SjhGY2xaaEhqanc6') 
+            .send(requestBody)
+            .expect(200);
 
-    //     expect(response.body.name).to.equal(requestBody.name);
-    //     expect(response.body.job).to.equal(requestBody.job);
+        console.log("Cek hasil post disini:", response.body)
 
-    //     const validate = ajv.compile(postSchema);
-    //     const valid = validate(response.body);
+        expect(response.body.reference_id).to.equal(requestBody.reference_id);
+        expect(response.body.bank_code).to.equal(requestBody.bank_code);
+        expect(response.body.bank_account_number).to.equal(requestBody.bank_account_number);
+
+        const validate = ajv.compile(postSchema);
+        const valid = validate(response.body);
         
-    //     if (!valid) {
-    //         console.log("Error Details:", validate.errors);
-    //     }
-    //     expect(valid).to.be.true;
-    // });
+        if (!valid) {
+            console.log("Error POST Details:", validate.errors);
+        }
+        expect(valid).to.be.true;
+    });
 });
